c2439941fd4d1695a112e8d6a32f8b3845dfbe6b
LoveTY0712 <3165944192@qq.com>
2024-10-01 17:14:04 +0800

    update

--- exercises/move_semantics/move_semantics1.rs
+++ exercises/move_semantics/move_semantics1.rs
@@ -5,11 +5,15 @@
 
 
 fn main() {
-    let vec0 = Vec::new();
-
+    let vec0: Vec<i32> = Vec::new();
+    let mut vec2=vec0.clone();
     let mut vec1 = fill_vec(vec0);
+    // let x=String::from("hello");
+    // let y=x.clone();
+    // println!("{x}");
+    vec2.push(22);
 
-    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec2", vec2.len(), vec2);
 
     vec1.push(88);
 
@@ -17,7 +21,7 @@ fn main() {
 }
 
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec:Vec<i32> = vec;
 
     vec.push(22);
     vec.push(44);
